e63891b9b7c2d20c2d4ebbc5e00370dc3ac1fcc4
Tristan Geronimo <tristanpgeronimo@gmail.com>
2022-06-17 10:45:05 +0200

    Add files via upload

---
+++ API-Interceptor02/src/main/java/it/triexercise/APIInterceptor02/ApiInterceptor02Application.java
@@ -0,0 +1,13 @@
+package it.triexercise.APIInterceptor02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInterceptor02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInterceptor02Application.class, args);
+	}
+
+}
---
+++ API-Interceptor02/src/test/java/it/triexercise/APIInterceptor02/ApiInterceptor02ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.triexercise.APIInterceptor02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInterceptor02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
